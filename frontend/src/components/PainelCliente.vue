d2b8014bbab17711f2c702096c6c60cf1851a591
Pablo <pabloalves.gbbs@gmail.com>
2024-02-06 21:55:19 -0300

    add painelcliente

--- frontend/src/components/PainelCliente.vue
+++ frontend/src/components/PainelCliente.vue
@@ -1,78 +1,15 @@
 <template>
-    <!-- <div class="flex h-screen">
-      <aside class="w-2/12 bg-blue-900 text-blue-100" aria-label="Sidebar">
-        <div class="overflow-y-auto py-4 px-3 h-full">
-          <div class="flex flex-col items-center">
-            <div class="mb-8">
-              <img src="your-logo-path.svg" alt="Your Logo" class="w-24 h-24">
-            </div>
-            <ul class="space-y-4 flex-1">
-              <li>
-                <a href="#" class="flex items-center p-2 text-lg rounded-lg hover:bg-blue-800 transition duration-150 ease-in-out">
-                  <svg class="w-6 h-6 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                  <span>Agendar Horário</span>
-                </a>
-              </li>
-              <li>
-                <a href="#" class="flex items-center p-2 text-lg rounded-lg hover:bg-blue-800 transition duration-150 ease-in-out">
-                  <svg class="w-6 h-6 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 19 7.5 19s3.332-.477 4.5-1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13c-1.168.776-2.753 1.253-4.5 1.253s-3.332-.477-4.5-1.253"></path></svg>
-                  <span>Configurações</span>
-                </a>
-              </li>
-              <li>
-                <a href="#" class="flex items-center p-2 text-lg rounded-lg hover:bg-blue-800 transition duration-150 ease-in-out">
-                  <svg class="w-6 h-6 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.364 5.636l1.414 1.414a9 9 0 11-12.728 0l1.414-1.414a7 7 0 1010.9 0zM15 11a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
-                  <span>Ajuda</span>
-                </a>
-              </li>
-              <li>
-                <a href="#" class="flex items-center p-2 text-lg rounded-lg hover:bg-blue-800 transition duration-150 ease-in-out">
-                  <svg class="w-6 h-6 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 3v4M3 5h4m1 4V2a1 1 0 011-1h10a1 1 0 011 1v14a1 1 0 01-1 1h-5m1 4h4a1 1 0 001-1v-4a1 1 0 00-1-1h-4a1 1 0 00-1 1v4a1 1 0 001 1zm-6 0h4a1 1 0 001-1v-4a1 1 0 00-1-1H9a1 1 0 00-1 1v4a1 1 0 001 1z"></path></svg>
-                  <span>Clientes</span>
-                </a>
-              </li>
-              <li>
-                <a href="#" class="flex items-center p-2 text-lg rounded-lg hover:bg-blue-800 transition duration-150 ease-in-out">
-                  <svg class="w-6 h-6 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
-                  <span>Calendários</span>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </aside>
-  </div> -->
 <div>
-
-    <div class="flex h-screen">
-        <aside class="w-2/12 bg-zinc-800 text-white py-10 px-8" aria-label="Sidebar">
-
-            <div class="flex justify-center items-center pb-10">
-                <h1 class="text-4xl font-semibold">Logo</h1>
-            </div>
-
-            <hr class="bg-">
-
-            <div>
-
-                <ul class="space-y-16 my-10 text-md">
-                    <li>Agendamento</li>
-                    <li>Clientes</li>
-                    <li>Configurações</li>
-                    <li>Calendario</li>
-                    <li>Serviços</li>
-                    <li>Ajuda</li>
-                </ul>
-
-            </div>
-
-        </aside>
-    </div>
+    <SideNav/>
 
 </div>
   </template>
   
   <script>
+  import SideNav from './SideNav.vue'
   export default {
+    components: {
+      SideNav
+    }
   };
   </script>
