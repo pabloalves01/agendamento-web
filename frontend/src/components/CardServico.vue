859ddddb1e7e62678cbd836df4e9fccbd78baa3d
Pablo <pabloalves.gbbs@gmail.com>
2024-02-13 11:54:29 -0300

    Update CardServico.vue

--- frontend/src/components/CardServico.vue
+++ frontend/src/components/CardServico.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="bg-white border w-5/12 h-auto py-5 px-10">
+  <div
+    class="bg-white border 6/12 h-auto py-5 px-10 cursor-pointer hover:bg-gray-50"
+  >
     <div class="flex flex-row justify-between">
       <span>{{ nomeCorte }}</span>
       <span>{{ duracao }}</span>
@@ -9,9 +11,12 @@
       <div
         v-for="(funcionario, index) in funcionarios"
         :key="index"
-        class="bg-gray-200 py-2.5 px-5 col-span-4 shadow-sm border border-gray-300 text-center cursor-pointer"
+        class="hover:bg-indigo-800 hover:text-white rounded-md py-2.5 px-5 col-span-4 shadow-sm border border-gray-300 text-left cursor-pointer"
       >
-        {{ funcionario.nome_funcionario }}
+        <div class="flex flex-row items-center ">
+          <i class="fa-regular fa-circle-user mr-2"></i>
+          {{ funcionario.nome_funcionario }}
+        </div>
       </div>
     </div>
   </div>
@@ -20,13 +25,9 @@
 <script>
 export default {
   name: "CardServico",
-  props: [
-    "nomeCorte",
-    "duracao",
-    "funcionarios"
-  ],
+  props: ["nomeCorte", "duracao", "funcionarios"],
   created() {
-    console.log("dados",this.funcionarios);
+    console.log("dados", this.funcionarios);
   },
 };
 </script>
