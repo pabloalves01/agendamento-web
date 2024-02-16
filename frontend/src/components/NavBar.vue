8ec09065a1d347a33c88d59e39b320559bdccb6d
Pablo Alves <pabloalves.gbbs@gmail.com>
2024-02-16 17:59:45 -0300

    adicionado carrinhos de comrpas

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <nav class="bg-indigo-800 border-b shadow-sm text-white py-6">
+    <nav class="bg-vermelho text-white py-6 z-50">
       <div
         class="container mx-auto flex items-center justify-between text-white"
       >
@@ -22,17 +22,101 @@
               <a href="#" class="text-md">Contato</a>
             </li>
           </ul>
-          <button
-            class="ml-6 py-2 px-4 text-white bg-pink-500 hover:bg-pink-400 rounded-sm focus:outline-none"
+          <div>
+            <button
+              class="mx-4 py-2 px-4 text-white bg-cinza rounded-full focus:outline-none hover:bg-zinc-800"
+            >
+              <span class="text-sm"> SEJA UM CLIENTE </span>
+            </button>
+            <button
+              class="mr-4 py-2 px-4 text-white bg-cinza rounded-full focus:outline-none hover:bg-zinc-800"
+              @click="this.$router.push('/painel-do-cliente')"
+            >
+              <span class="text-sm"> PORTAL DO CLIENTE </span>
+            </button>
+          </div>
+
+          <div
+            class="bg-cinza rounded-full w-32 h-10 flex items-center px-4 gap-2 hover:bg-zinc-800 transition-all duration-400"
+            @click="toggleModal"
           >
-            Seja um cliente!
-          </button>
-          <button
-            class="ml-6 py-2 px-4 text-white bg-pink-500 hover:bg-pink-400 rounded-sm focus:outline-none"
-            @click="this.$router.push('/painel-do-cliente')"
-          >
-            Portal do Cliente
-          </button>
+            <i class="fa-solid fa-cart-shopping"></i>
+            <span class="text-sm">CARRINHO</span>
+          </div>
+
+          <!-- MODAL DO CARRINHO -->
+
+          <transition name="slide">
+            <div
+              v-if="isCartVisible"
+              class="flex flex-col fixed right-0 top-36 w-80 h-auto bg-white shadow-lg z-40"
+            >
+              <!-- HEADER -->
+              <div class="p-4 bg-gray-100 flex-shrink-0">
+                <h2 class="text-lg text-black font-semibold">Seu Carrinho</h2>
+                <button
+                  class="absolute top-2 right-2 text-xl text-vermelho"
+                  @click="toggleModal"
+                >
+                  ×
+                </button>
+              </div>
+              <!-- FIM DO HEADER -->
+
+              <!-- PRODUTOS -->
+              <div
+                v-if="produtos.length == 0"
+                class="flex flex-col text-center justify-center py-10 text-cinza"
+              >
+                <i class="text-4xl mb-2 fa-solid fa-cart-arrow-down"></i>
+                <p class="font-semibold">O seu carrinho está vazio</p>
+                <p class="text-cinza">Adicione um produto para continuar</p>
+              </div>
+
+              <div
+                v-else
+                v-for="(produto, index) in produtos"
+                :key="index"
+                class="flex flex-col gap-2 overflow-auto p-4"
+              >
+                <div
+                  class="border border-gray-200 hover:bg-gray-100 grid grid-cols-[auto_1fr] items-center gap-2"
+                >
+                  <!-- Foto do Produto -->
+                  <div
+                    class="aspect-square w-20 bg-red-500 flex items-center justify-center overflow-hidden"
+                  >
+                    <img
+                      src="url_da_imagem_aqui"
+                      alt="Nome do Produto"
+                      class="w-full h-full object-cover"
+                    />
+                  </div>
+
+                  <!-- Detalhes do Produto -->
+                  <div class="text-cinza p-2">
+                    <p class="text-sm font-bold">{{ produto.nome_produto }}</p>
+                    <p class="text-sm font-semibold">R$ {{ produto.preco }}</p>
+                  </div>
+                </div>
+              </div>
+              <!-- FIM DOS PRODUTOS -->
+
+              <!-- FOOTER -->
+              <div class="bg-gray-100 mt-auto flex-shrink-0 px-4">
+                <div class="flex justify-between my-4 text-black">
+                  <span class="text-sm">Total</span>
+                  <span class="text-sm">R$ 0,00</span>
+                </div>
+                <button
+                  class="w-full bg-vermelho text-white py-2 rounded-full mb-4"
+                >
+                  Finalizar Compra
+                </button>
+              </div>
+              <!-- FIM DO FOOTER -->
+            </div>
+          </transition>
         </div>
       </div>
     </nav>
@@ -42,5 +126,40 @@
 <script>
 export default {
   name: "NavBar",
+  data() {
+    return {
+      isCartVisible: false,
+      isButtonCartVisible: false,
+      isLogged: true,
+      produtos: [
+        {
+          codpro: 1,
+          nome_produto: "Produto Teste",
+          preco: 10.0,
+          imagem: "url_da_imagem_aqui",
+        },
+      ],
+    };
+  },
+
+  methods: {
+    toggleModal() {
+      if (this.isLogged || this.isButtonCartVisible) {
+        this.isCartVisible = !this.isCartVisible;
+      } // ajustar o login
+    },
+  },
 };
 </script>
+
+<style>
+/* Estilos da transição 'slide' */
+.slide-enter-active,
+.slide-leave-active {
+  transition: transform 0.3s ease;
+}
+.slide-enter-from,
+.slide-leave-to {
+  transform: translateX(100%);
+}
+</style>
