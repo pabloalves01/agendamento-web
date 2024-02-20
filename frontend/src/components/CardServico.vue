de80e13512bd60a608dd1959b47b66c5ef01eeaf
Pablo Alves <pabloalves.gbbs@gmail.com>
2024-02-20 14:04:23 -0300

    Update CardServico.vue

--- frontend/src/components/CardServico.vue
+++ frontend/src/components/CardServico.vue
@@ -1,70 +1,72 @@
 <template>
   <div
-    class="border border-gray-300 bg-gray-100 px-6 py-4 w-full text-cinza rounded-sm cursor-pointer hover:bg-gray-200 transition duration-300 ease-in-out"
+    class="border border-gray-300 bg-gray-100 w-full text-cinza rounded-sm cursor-pointer hover:bg-gray-200 transition duration-300 ease-in-out mb-5"
   >
-    <div class="flex justify-between">
-      <div>
-        <i class="fa-solid fa-scissors mr-2"></i>
-        <span class="font-semibold"> {{ nomeCorte }}</span>
-      </div>
-      <div class="flex gap-10">
+    <div class="px-6 py-4">
+      <div class="flex justify-between">
         <div>
-          <i class="fa-solid fa-clock mr-2"></i>
-          <span> {{ duracao }}</span>
+          <i class="fa-solid fa-scissors mr-2"></i>
+          <span class="font-semibold"> {{ nomeCorte }}</span>
         </div>
-        <div>
-          <i class="fa-solid fa-cart-shopping mr-2"></i>
-          <span> {{ valor }}</span>
-        </div>
-      </div>
-    </div>
-    <hr class="my-4" />
-    <div class="flex">
-      <div @click="toggleDropdown" class="cursor-pointer">
-        <div
-          id="funcionarios-disponiveis"
-          class="border border-gray-500 py-2 px-2 hover:bg-vermelho hover:text-white rounded-sm"
-        >
-          Pablo
+        <div class="flex gap-10">
+          <div>
+            <i class="fa-solid fa-clock mr-2"></i>
+            <span> {{ duracao }}</span>
+          </div>
+          <div>
+            <i class="fa-solid fa-cart-shopping mr-2"></i>
+            <span> {{ valor }}</span>
+          </div>
         </div>
       </div>
-    </div>
+      <hr class="my-4" />
+      <div class="flex">
+        <div @click="toggleDropdown" class="cursor-pointer">
+          <div
+            id="funcionarios-disponiveis"
+            class="border border-gray-500 py-2 px-2 hover:bg-vermelho hover:text-white rounded-sm"
+          >
+            Pablo
+          </div>
+        </div>
+      </div>
 
-    <div v-if="showDropdown">
-      <hr class="my-4" />
-      <div class="mb-2">
-        <span class="font-semibold">Manha</span>
-      </div>
-      <div class="flex gap-2">
-        <div
-          v-for="horarioManha in horariosManha"
-          :key="horarioManha"
-          class="border border-gray-500 py-2 px-2 hover:bg-vermelho hover:text-white rounded-sm"
-        >
-          {{ horarioManha }}
+      <div v-if="showDropdown">
+        <hr class="my-4" />
+        <div class="mb-2">
+          <span class="font-semibold">Manha</span>
         </div>
-      </div>
-      <hr class="my-4" />
-      <div class="mb-2">
-        <span class="font-semibold">Tarde</span>
-      </div>
-      <div class="flex gap-2">
-        <div
-          v-for="horarioTarde in horariosTarde"
-          :key="horarioTarde"
-          class="border border-gray-500 py-2 px-2 hover:bg-vermelho hover:text-white rounded-sm"
-        >
-          {{ horarioTarde }}
+        <div class="flex gap-2">
+          <div
+            v-for="horarioManha in horariosManha"
+            :key="horarioManha"
+            class="border border-gray-500 py-2 px-2 hover:bg-vermelho hover:text-white rounded-sm"
+          >
+            {{ horarioManha }}
+          </div>
+        </div>
+        <hr class="my-4" />
+        <div class="mb-2">
+          <span class="font-semibold">Tarde</span>
+        </div>
+        <div class="flex gap-2">
+          <div
+            v-for="horarioTarde in horariosTarde"
+            :key="horarioTarde"
+            class="border border-gray-500 py-2 px-2 hover:bg-vermelho hover:text-white rounded-sm"
+          >
+            {{ horarioTarde }}
+          </div>
         </div>
       </div>
     </div>
-  </div>
-  <div
-    v-if="showDropdown"
-    class="bg-cinza hover:bg-green-500 transition duration-300 ease-in-out w-full py-2 flex items-center gap-1 justify-center text-white font-semibold mb-10 rounded-b-sm cursor-pointer mb-2"
-  >
-    <i class="fa-solid fa-check"></i>
-    <span>AGENDAR HORARIO</span>
+    <div
+      v-if="showDropdown"
+      class="bg-cinza hover:bg-green-500 transition duration-300 ease-in-out w-full py-2 flex items-center gap-1 justify-center text-white font-semibold rounded-b-sm cursor-pointer"
+    >
+      <i class="fa-solid fa-check"></i>
+      <span>AGENDAR HORARIO</span>
+    </div>
   </div>
 </template>
 
