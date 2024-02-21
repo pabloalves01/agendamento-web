36fec42b8d5e07bd830bb8d22aefea83906e0de6
Pablo <pabloalves.gbbs@gmail.com>
2024-02-20 22:31:25 -0300

    remaster painel do cliente

--- frontend/src/components/PainelCliente.vue
+++ frontend/src/components/PainelCliente.vue
@@ -1,38 +1,26 @@
 <template>
   <div>
-    <SideNav />
-    <div class="p-4 sm:ml-64">
-      <div
-        class="p-4 h-full border-gray-200 rounded-lg dark:border-gray-700 mt-14"
-      >
-        <div class="mt-8">
-          <span class="text-4xl font-semibold text-zinc-800"
-            >Gerenciamento de Funcionarios</span
-          >
-          <p class="mt-2">
-            Cadastre, exclua ou altere os funcionários do seu negócio de maneira
-            simplificada.
-          </p>
-        </div>
+    <HeaderComponent />
+      <NavBar />
+    <aside class="border border-gray-300   w-80 h-screen p-5">
+      <h2 class="text-cinza font-bold text-xl">My Side Nav</h2>
+    </aside>
 
-
-        <div>
-
-          <TableComponent />
-        </div>
-      </div>
-    </div>
   </div>
 </template>
 
 <script>
-import SideNav from "./SideNav.vue";
-import FuncionariosPainel from "./FuncionariosPainel.vue";
+import HeaderComponent from "./Layout/HeaderComponent.vue";
+import NavBar from "./Layout/NavBar.vue";
+
 export default {
   components: {
-    SideNav,
-    TableComponent: FuncionariosPainel,
-    
-},
+    HeaderComponent,
+    NavBar,
+  },
 };
 </script>
+
+<style>
+/* Add any additional styles here */
+</style>
