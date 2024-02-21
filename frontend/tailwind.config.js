55a566a48bcf133a562272ce3ee7281da587dfdb
Pablo Alves <pabloalves.gbbs@gmail.com>
2024-02-21 17:10:14 -0300

    container max 1200

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-  purge: [],
-  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"], // Removi o purge duplicado
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
@@ -8,13 +7,23 @@ module.exports = {
         vermelho: "#EC5656",
         cinza: "#202020",
       },
+      // Configuração do container adicionada aqui
+      container: {
+        center: true,
+        padding: "1rem", // Ajuste o padding conforme necessário
+        screens: {
+          sm: "640px", // Define o tamanho máximo para telas pequenas, se necessário
+          md: "768px", // Define o tamanho máximo para telas médias, se necessário
+          lg: "1200px", // Aqui é onde definimos o tamanho máximo de 1200px para telas grandes
+          xl: "1200px", // Mantém o tamanho máximo de 1200px para telas extra grandes
+          "2xl": "1200px", // Mantém o tamanho máximo de 1200px para telas 2xl
+        },
+      },
     },
   },
   variants: {
     extend: {
-      maxWidth: {
-        custom: "100rem",
-      },
+      // Suas extensões de variantes aqui
     },
   },
   plugins: [],
