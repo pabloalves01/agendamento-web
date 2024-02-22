ddede095ae9d926e6d60179f937bb1f020d5fd2c
Pablo Alves <pabloalves.gbbs@gmail.com>
2024-02-22 14:16:29 -0300

    Update PainelCliente.vue

--- frontend/src/components/PainelCliente.vue
+++ frontend/src/components/PainelCliente.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="flex flex-col md:flex-row">
     <aside
-      class="border border-gray-300 w-80 h-screen fixed top-0 left-0 py-6 px-5 z-10"
+      class="border border-gray-300 w-80 h-screen fixed top-0 left-0 py-6 z-10"
     >
-      <div id="cabecalho" class="flex">
-        <div class="bg-cinza h-16 w-20"></div>
+      <div id="cabecalho" class="flex pl-5">
         <div class="p-2 w-full flex-col">
           <span class="text-base"
             >Olá, <span class="font-semibold">Pablo Alves!</span></span
@@ -15,79 +14,86 @@
         </div>
       </div>
 
-      <hr class="my-5" />
+      <hr class="my-2" />
 
-      <ul>
-        <li
-          class="flex items-center gap-2 cursor-pointer hover:bg-gray-200 py-2"
-        >
-          <CalendarCheck :size="22" />Agenda
-        </li>
-        <hr class="my-2" />
-        <li
-          class="flex items-center gap-2 cursor-pointer hover:bg-gray-200/50 py-2"
-        >
-          <HandCoins :size="22" />Comissões
-        </li>
-        <hr class="my-2" />
+      <div>
+        <ul>
+          <div class="py-2 pl-5">
+            <span class="text-sm font-semibold text-cinza">Seu Negócio</span>
+          </div>
 
-        <li
-          class="flex items-center gap-2 cursor-pointer hover:bg-gray-200/50 py-2"
-        >
-          <Contact2 :size="22" />Profissionais
-        </li>
-        <hr class="my-2" />
+          <div class="pl-5">
+            <li
+              class="flex items-center gap-2 cursor-pointer hover:bg-gray-200 py-3 text-sm"
+            >
+              <CalendarCheck :size="22" />Agenda
+            </li>
+            <li
+              class="flex items-center gap-2 cursor-pointer hover:bg-gray-200/50 py-3 text-sm"
+            >
+              <Scissors :size="22" />Serviços
+            </li>
+            <li
+              class="flex items-center gap-2 cursor-pointer hover:bg-gray-200/50 py-3 text-sm"
+            >
+              <ShoppingBasket :size="22" />Produtos
+            </li>
 
-        <li
-          class="flex items-center gap-2 cursor-pointer hover:bg-gray-200/50 py-2"
-        >
-          <Scissors :size="22" />Serviços
-        </li>
-        <hr class="my-2" />
+            <li
+              class="flex items-center gap-2 cursor-pointer hover:bg-gray-200/50 py-3 text-sm"
+            >
+              <Handshake :size="22" />Clientes
+            </li>
+          </div>
+          <hr class="my-2" />
 
-        <li
-          class="flex items-center gap-2 cursor-pointer hover:bg-gray-200/50 py-2"
-        >
-          <ShoppingBasket :size="22" />Produtos
-        </li>
-        <hr class="my-2" />
+          <div class="pl-5">
+            <div class="py-2">
+              <span class="text-sm font-semibold text-cinza"
+                >Administração</span
+              >
+            </div>
 
-        <li
-          class="flex items-center gap-2 cursor-pointer hover:bg-gray-200/50 py-2"
-        >
-          <Handshake :size="22" />Clientes
-        </li>
-        <hr class="my-2" />
+            <li
+              class="flex items-center gap-2 cursor-pointer hover:bg-gray-200/50 py-3 text-sm"
+            >
+              <HandCoins :size="22" />Comissões
+            </li>
+            <li
+              class="flex items-center gap-2 cursor-pointer hover:bg-gray-200/50 py-3 text-sm"
+            >
+              <Contact2 :size="22" />Profissionais
+            </li>
 
-        <li
-          class="flex items-center gap-2 cursor-pointer hover:bg-gray-200/50 py-2"
-        >
-          <CircleDollarSign :size="22" />Meu Plano
-        </li>
-        <hr class="my-2" />
+            <li
+              class="flex items-center gap-2 cursor-pointer hover:bg-gray-200/50 py-3 text-sm"
+            >
+              <CircleDollarSign :size="22" />Meu Plano
+            </li>
 
-        <li
-          class="flex items-center gap-2 cursor-pointer hover:bg-gray-200/50 py-2"
-        >
-          <Wallet :size="22" />Fluxo de Caixa
-        </li>
-        <hr class="my-2" />
+            <li
+              class="flex items-center gap-2 cursor-pointer hover:bg-gray-200/50 py-3 text-sm"
+            >
+              <Wallet :size="22" />Fluxo de Caixa
+            </li>
 
-        <li
-          class="flex items-center gap-2 cursor-pointer hover:bg-gray-200/50 py-2"
-        >
-          <Gauge :size="22" />Fluxo de Caixa
-        </li>
-        <hr class="my-2" />
+            <li
+              class="flex items-center gap-2 cursor-pointer hover:bg-gray-200/50 py-3 text-sm"
+            >
+              <Gauge :size="22" />Fluxo de Caixa
+            </li>
+          </div>
+          <hr class="my-2" />
 
-        <li><button class="text-red-500">Sair</button></li>
-      </ul>
+          <li><button class="text-red-500 py-3 text-sm ml-5">Sair</button></li>
+        </ul>
+      </div>
     </aside>
 
     <div class="w-80"></div>
 
     <header
-      class="flex items-center border border-b-gray-300 w-full py-3.5 fixed top-0 left-80 right-0 z-10"
+      class="flex items-center border border-b-gray-300 w-full py-2.5 fixed top-0 left-80 right-0 z-10"
     >
       <div class="px-6 w-full flex flex-row items-center">
         <input
@@ -97,7 +103,7 @@
         />
       </div>
     </header>
-    <main class="flex-1">teste</main>
+    <main class="bg-red-200 mt-16 w-full h-96">teste</main>
   </div>
 </template>
 
