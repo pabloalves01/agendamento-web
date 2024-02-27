2c03ace60e13b85a56c07cc56ee3cae9c6402ac5
Pablo Alves <139909793+pabloalves01@users.noreply.github.com>
2024-02-27 12:00:56 -0300

    Update CardTotais.vue

--- frontend/src/components/Cards/CardTotais.vue
+++ frontend/src/components/Cards/CardTotais.vue
@@ -1,29 +1,66 @@
 <template>
-
-    <div class="px-6 py-8 border border-gray-300  rounded-md flex items-center gap-4">
-
+    <div>
+      <div
+        v-for="(stat, index) in stats"
+        :key="index"
+        class="px-6 py-8 border border-gray-300 rounded-md flex flex-row w-96 items-center gap-4"
+      >
         <div><UserRoundCheck size="40" /></div>
         <div class="flex flex-col">
-            <span class="text-sm">Total de Clientes</span>
-            <span class="text-lg font-semibold">999</span>
+          <span class="text-sm">{{ stat.title }}</span>
+          <span class="text-lg font-semibold">{{ stat.value }}</span>
         </div>
-
-
+      </div>
     </div>
-</template>
-
-<script>
-import { UserRoundCheck } from 'lucide-vue-next';
-export default {
-  name: "CardTotais",
-  components: {
-    UserRoundCheck,
-  },
-  props: {
-    title: {
-      type: String,
-      required: true,
+  </template>
+  
+  <script>
+  import { UserRoundCheck } from 'lucide-vue-next';
+  
+  export default {
+    name: "CardTotais",
+    components: {
+      UserRoundCheck,
     },
-  },
-};
-</script>
+    data() {
+      return {
+        stats: [
+          {
+            title: "Total de Clientes",
+            value: "999",
+          },
+          {
+            title: "Total de Funcionários",
+            value: "999",
+          },
+          {
+            title: "Total de Serviços",
+            value: "999",
+          },
+          {
+            title: "Total de Produtos",
+            value: "999",
+          },
+          {
+            title: "Total de Vendas",
+            value: "999",
+          },
+          {
+            title: "Total de Agendamentos",
+            value: "999",
+          },
+          {
+            title: "Total de Faturamento",
+            value: "R$ 999",
+          },
+        ],
+      };
+    },
+    props: {
+      title: {
+        type: String,
+        required: true,
+      },
+    },
+  };
+  </script>
