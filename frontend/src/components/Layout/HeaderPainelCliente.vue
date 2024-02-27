2014251f81856d1352daf02389907892f1f38ca8
Pablo Alves <139909793+pabloalves01@users.noreply.github.com>
2024-02-27 16:01:53 -0300

    Update HeaderPainelCliente.vue

--- frontend/src/components/Layout/HeaderPainelCliente.vue
+++ frontend/src/components/Layout/HeaderPainelCliente.vue
@@ -3,10 +3,9 @@
     class="flex items-center border border-b-gray-300 w-full bg-white py-5 sticky top-0 left-80 z-50"
   >
     <div class="px-6 w-full flex flex-row items-center justify-between">
-      <!-- Adicionei justify-between aqui -->
       <input
         type="text"
-        class="border border-gray-300 bg-transparent rounded-sm py-2 placeholder-gray-400 w-96 px-4 focus:outline-none focus:ring-2 focus:ring-cinza focus:border-transparent transition-all duration-200 ease-in-out"
+        class="border border-gray-300 bg-transparent rounded-lg py-2 placeholder-gray-400 w-96 px-4 focus:outline-none focus:ring-2 focus:ring-cinza focus:border-transparent transition-all duration-200 ease-in-out"
         placeholder="Buscar por cliente, produto, serviço..."
       />
       <div class="flex gap-2 items-center">
